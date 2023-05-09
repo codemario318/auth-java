cee52e530a7dfeede8a8f0d9e70f60d135ee0bb0
mario <codemario318@gmail.com>
2023-05-09 17:12:33 +0900

    [feature-signup] 회원가입 기능 구현 ADD | 회원가입 API 1차 구현

---
+++ src/main/java/com/mario/auth/dto/ExceptionResponse.java
@@ -0,0 +1,12 @@
+package com.mario.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ExceptionResponse {
+    private String message;
+}
